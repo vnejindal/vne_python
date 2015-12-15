1120aa663fb172a41311f89faa67bfbd6d5f545b
Vinay Jindal <vinay.jindal@gmail.com>
2015-12-15 12:32:26 +0530

    More scripts added

--- google-python-exercises/babynames/babynames.py
+++ google-python-exercises/babynames/babynames.py
@@ -9,6 +9,9 @@
 import sys
 import re
 
+summary = False 
+summary_file = ''
+
 """Baby Names exercise
 
 Define the extract_names() function below and change main()
@@ -26,6 +29,8 @@ Here's what the html looks like in the baby.html files:
 <tr align="right"><td>3</td><td>Matthew</td><td>Brittany</td>
 ...
 
+<td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>
+
 Suggested milestones for incremental development:
  -Extract the year and print it
  -Extract the names and rank numbers and just print them
@@ -41,8 +46,60 @@ def extract_names(filename):
   ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
   """
   # +++your code here+++
-  return
+  # Extract year from input filename and add it to return list
+  # Now extract each name with rank, create a new string and append 
+  # it to another list
+  # Sort this one and add the result to final return list 
+  
+  # Open file name and start reading lines 
 
+  fobj = open(filename, 'r')
+  if fobj.closed is not False : 
+    print "File %s does not exist"  %(fobj.name())
+    sys.exit(0)
+
+  if summary is True : 
+    fw_obj = open(summary_file, 'a')
+    if fw_obj.closed is not False : 
+      print "File %s does not exist"  %(fw_obj.name())
+      sys.exit(0)
+
+  name_dict = {}
+  year_str = ''	
+  for f_line in fobj : 
+    # Parse the file to get year  
+#    match = re.search(r'Popularity in (\d\d\d\d)', f_line)
+    match = re.search(r'Popularity in (\d{4})', f_line)
+
+    if match : 
+      #check if this year exists in dictionary, if not add it
+      if match.group(1) not in name_dict : 
+        name_dict[match.group(1)] = []
+        year_str = match.group(1)
+    match = re.search(r'<td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>', f_line)
+    if match : 
+      #print match.group(1), match.group(2), match.group(3)
+      str_1 = match.group(2) + ' ' + match.group(1)
+      str_2 = match.group(3) + ' ' + match.group(1)
+      name_dict[year_str].append(str_1)
+      name_dict[year_str].append(str_2)
+
+  fw_obj.write(year_str)
+  num_names = 0
+  sorted_list = sorted(name_dict[year_str])
+  if summary is True :
+    for name_str in sorted_list : 
+      fw_obj.write(' ' + name_str)
+      num_names += 1
+  
+  fw_obj.write('\n')
+  
+  print "Written %d entries for year %s" %(num_names, year_str)
+  fobj.close()
+  if summary is True :
+    fw_obj.close()
+#vne:: look for solution directory as a different implementation using tuples  
+  return
 
 def main():
   # This command-line parsing code is provided.
@@ -55,14 +112,19 @@ def main():
     sys.exit(1)
 
   # Notice the summary flag and remove it from args if it is present.
-  summary = False
+  global summary 
+  global summary_file 
   if args[0] == '--summaryfile':
     summary = True
     del args[0]
-
+    summary_file = args[0]
+    del args[0]
   # +++your code here+++
   # For each filename, get the names, then either print the text output
   # or write it to a summary file
+  for filename in args : 
+    extract_names(filename)
+#  extract_names(args[0])
   
 if __name__ == '__main__':
   main()
--- google-python-exercises/copyspecial/copyspecial.py
+++ google-python-exercises/copyspecial/copyspecial.py
@@ -11,15 +11,76 @@ import re
 import os
 import shutil
 import commands
+import subprocess
 
 """Copy Special exercise
+  The copyspecial.py program takes one or more directories as its arguments. 
+  We'll say that a "special" file is one where the name contains the pattern __w__ somewhere, 
+  where the w is one or more word chars. The provided main() includes code to parse the 
+  command line arguments, but the rest is up to you. Write functions to implement the 
+  features below and modify main() to call your functions.
+
+  Suggested functions for your solution(details below):
+
+  get_special_paths(dir) -- returns a list of the absolute paths of the special files in the given directory
+  copy_to(paths, dir) given a list of paths, copies those files into the given directory
+  zip_to(paths, zippath) given a list of paths, zip those files up into the given zipfile
 """
 
 # +++your code here+++
 # Write functions and modify main() to call them
+# get_special_paths(dir) -- 
+#    returns a list of the absolute paths of the special files in the given directory
 
+def get_special_paths(dir): 
+  print "Listing files in", dir
+  sp_file_list = []
+  files = os.listdir(dir)
+  for filename in files: 
+    #print filename
+    match = re.search(r'.*__\w+__.*',filename)
+    if match :
+      #print os.path.join(dir,match.group())
+      #print os.path.abspath(os.path.join(dir,match.group()))
+      sp_file_list.append(os.path.abspath(os.path.join(dir,match.group())))
+  
+  return sp_file_list
 
+# copy_to(paths, dir) - 
+#     given a list of paths, copies those files into the given directory
+def copy_to(paths, dir):
+  count = 0
+  # Check if 'dir' exists
+  if not os.path.exists(dir) : 
+    os.mkdir(dir) 
+	
+  for path in paths: 
+    shutil.copy(path, dir)
+    count += 1
+  print count, "files copied"
+  return   
+#   zip_to(paths, zippath) - 
+#        given a list of paths, zip those files up into the given zipfile
+def zip_to(paths, zippath): 
 
+  if os.path.exists(zippath): 
+    os.rmdir(zippath)
+  cmd_line = 'tar -cvf ' + zippath + ' ' + ' '.join(paths)
+
+  print cmd_line	
+  # using subprocess module instead  
+  subprocess.call(cmd_line)
+  return 
+
+'''
+  (status, output) = commands.getstatusoutput(cmd_line)
+  if status: 
+    print "Error" 
+    sys.stderr.write(output)
+    sys.exit(1)
+  print "Success:", status, output 
+'''
+  
 def main():
   # This basic command line argument parsing code is provided.
   # Add code to call your functions below.
@@ -38,7 +99,7 @@ def main():
   if args[0] == '--todir':
     todir = args[1]
     del args[0:2]
-
+	
   tozip = ''
   if args[0] == '--tozip':
     tozip = args[1]
@@ -50,6 +111,16 @@ def main():
 
   # +++your code here+++
   # Call your functions
+  # vne:: tbd dirs parsing 
+  if todir is not '' : 
+    sp_file_list = get_special_paths(args[0])
+    copy_to(sp_file_list, todir)
+
+  if tozip is not '' : 
+    sp_file_list = get_special_paths(args[0])
+    zip_to(sp_file_list, tozip)
+
+  #sp_file_list = get_special_paths(args[0])
   
 if __name__ == "__main__":
   main()
